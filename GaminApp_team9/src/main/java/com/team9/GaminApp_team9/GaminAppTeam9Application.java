22ef2de71b4d7659687003992c468ba8d90ebb07
sowjanya123-cpu <2023cs_sowjanyanu_d@nie.ac.in>
2025-09-17 09:37:46 +0530

    Added GaminApp_team9 project

---
+++ GaminApp_team9/src/main/java/com/team9/GaminApp_team9/GaminAppTeam9Application.java
@@ -0,0 +1,13 @@
+package com.team9.GaminApp_team9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaminAppTeam9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaminAppTeam9Application.class, args);
+	}
+
+}
